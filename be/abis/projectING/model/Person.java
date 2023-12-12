2f817a126acf625bc188dd9accd3cacaece5fc55
uh23wb <rajesh.kallayel.mohanan@ing.com>
2023-12-12 14:06:46 +0100

    updated details

--- be/abis/projectING/model/Person.java
+++ be/abis/projectING/model/Person.java
@@ -1,4 +1,16 @@
 package be.abis.projectING.model;
 
-public class Person {
+abstract class Person{
+    public abstract void Personorder();
 }
+
+abstract class Admin extends Person{
+   public abstract void Personorder();
+}
+
+abstract class Participant extends Person{
+
+    public abstract void Personorder();
+}
+
+
