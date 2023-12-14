11a7d03d5739fd60760234eb7b81692333063aaa
KaramK1 <153208677+KaramK1@users.noreply.github.com>
2023-12-14 13:57:20 +0100

    Create OrderService.java

---
+++ be/abis/projectING/services/OrderService.java
@@ -0,0 +1,34 @@
+package services;
+
+import model.Order;
+import model.Sandwich;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderService {
+    private Map<String, List<Order>> userOrders;
+    private SandwichService sandwichService; // Added field
+
+    public OrderService(SandwichService sandwichService) {
+        this.sandwichService = sandwichService;
+        userOrders = new HashMap<>();
+    }
+
+    public boolean placeOrder(String userName, Sandwich sandwich) {
+        // Check if the user has already placed 2 orders
+        if (getOrdersByUser(userName).size() >= 2) {
+            return false; // Exceeded order limit
+        }
+
+        Order order = new Order(userName, sandwich);
+        userOrders.computeIfAbsent(userName, k -> new ArrayList<>()).add(order);
+        return true; // Order placed successfully
+    }
+
+    public List<Order> getOrdersByUser(String userName) {
+        return userOrders.getOrDefault(userName, new ArrayList<>());
+    }
+}
