088d07b38d3c9a21468b22a6dd331c93980210a7
KaramK1 <153208677+KaramK1@users.noreply.github.com>
2023-12-14 13:51:25 +0100

    Update Person.java

--- be/abis/projectING/model/Person.java
+++ be/abis/projectING/model/Person.java
@@ -1,6 +1,33 @@
-package be.abis.projectING.model;
-import java.util.*;
+package model;
 
 public class Person {
+    private String name;
+    private String role;
+    private Course course;
 
+    // Use the constructor for the Persontest without the course
+    public Person(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Person(String name, String role, Course course) {
+        this.name = name;
+        this.role = role;
+        this.course = course;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
 }
+
