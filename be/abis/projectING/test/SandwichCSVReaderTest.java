35f67a637de484fd8c71f1abdbc7597687634580
KaramK1 <153208677+KaramK1@users.noreply.github.com>
2023-12-14 14:03:31 +0100

    Create SandwichCSVReaderTest.java

---
+++ be/abis/projectING/test/SandwichCSVReaderTest.java
@@ -0,0 +1,16 @@
+package test;
+import org.junit.Test;
+import util.SandwichCSVReader;
+
+import java.io.IOException;
+
+public class SandwichCSVReaderTest {
+
+    @Test
+    public void testCSVReader() throws IOException {
+        SandwichCSVReader.main(null); // Call your CSV reader class
+
+    }
+
+
+}
